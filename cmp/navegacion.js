439c5f7dec6160b45f29cd4d582ba4d14c7dc7f7
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2023-05-15 17:31:11 -0600

    update

--- cmp/navegacion.js
+++ cmp/navegacion.js
@@ -4,7 +4,7 @@ class Cnavegacion extends HTMLElement {
       `<ul>
       <li><a href="index.html">Inicio</a></li>
       <li><a href="micuenta.html">Mi cuenta</a></li>
-      <li><a href="#servicios">Terapeutas</a></li>
+      <li><a href="terapeutas.html">Terapeutas</a></li>
       <li><a href="#contacto">Pacientes</a></li>
       <li><a href="#contacto">Vinculación</a></li>
       </ul>`;
